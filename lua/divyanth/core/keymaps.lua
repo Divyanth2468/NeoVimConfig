c274132f62d479f259e52896e10c9657dbae1236
DivyanthSatya <80347640+Divyanth2468@users.noreply.github.com>
2026-02-21 21:35:11 +0530

    feat: added tmux pane switching and disabled nvim terminal

--- lua/divyanth/core/keymaps.lua
+++ lua/divyanth/core/keymaps.lua
@@ -24,32 +24,32 @@ keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" })     
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" })    -- go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" })
 
--- terminal key maps
-keymap.set("t", "<Esc>", "<C-\\><C-n>", { silent = true, desc = "Exit terminal mode" })
-keymap.set("n", "<leader>th", "<cmd>split | terminal<CR>", { silent = true, desc = "Terminal horizontal" })
-keymap.set("n", "<leader>tv", "<cmd>vsplit | terminal<CR>", { silent = true, desc = "Terminal vertical" })
-keymap.set("n", "<leader>tg", "<cmd>tabnew | terminal<CR>", { silent = true, desc = "Terminal group (new tab)" })
-keymap.set("n", "<leader>q", "<cmd>close<CR>", { silent = true, desc = "Close window" })
-
--- TERMINAL TOGGLE (HORIZONTAL, REUSED)
-local term_win = nil
-local function toggle_terminal()
-  if term_win and vim.api.nvim_win_is_valid(term_win) then
-    vim.api.nvim_win_close(term_win, true)
-    term_win = nil
-  else
-    vim.cmd("split | terminal")
-    term_win = vim.api.nvim_get_current_win()
-    vim.cmd("startinsert")
-  end
-end
-keymap.set("n", "<leader>tt", toggle_terminal, { desc = "Toggle terminal" })
-
--- TERMINAL POLISH
-vim.api.nvim_create_autocmd("TermOpen", {
-  callback = function()
-    vim.opt_local.number = false
-    vim.opt_local.relativenumber = false
-    vim.cmd("startinsert")
-  end,
-})
+-- -- terminal key maps
+-- keymap.set("t", "<Esc>", "<C-\\><C-n>", { silent = true, desc = "Exit terminal mode" })
+-- keymap.set("n", "<leader>th", "<cmd>split | terminal<CR>", { silent = true, desc = "Terminal horizontal" })
+-- keymap.set("n", "<leader>tv", "<cmd>vsplit | terminal<CR>", { silent = true, desc = "Terminal vertical" })
+-- keymap.set("n", "<leader>tg", "<cmd>tabnew | terminal<CR>", { silent = true, desc = "Terminal group (new tab)" })
+-- keymap.set("n", "<leader>q", "<cmd>close<CR>", { silent = true, desc = "Close window" })
+--
+-- -- TERMINAL TOGGLE (HORIZONTAL, REUSED)
+-- local term_win = nil
+-- local function toggle_terminal()
+--   if term_win and vim.api.nvim_win_is_valid(term_win) then
+--     vim.api.nvim_win_close(term_win, true)
+--     term_win = nil
+--   else
+--     vim.cmd("split | terminal")
+--     term_win = vim.api.nvim_get_current_win()
+--     vim.cmd("startinsert")
+--   end
+-- end
+-- keymap.set("n", "<leader>tt", toggle_terminal, { desc = "Toggle terminal" })
+--
+-- -- TERMINAL POLISH
+-- vim.api.nvim_create_autocmd("TermOpen", {
+--   callback = function()
+--     vim.opt_local.number = false
+--     vim.opt_local.relativenumber = false
+--     vim.cmd("startinsert")
+--   end,
+-- })
